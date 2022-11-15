7cdf7b6afccee97f4cb3b6c8486fa3759207d8b5
Enki0189 <marudiaz0189@gmail.com>
2022-11-14 21:47:51 -0300

    Se crean objetos.

---
+++ contacto.py
@@ -0,0 +1,15 @@
+class Contacto:
+
+    def __init__(self,formulario_html):
+        # ormulario_hmtl = 
+        self.id_dni = formulario_html['id_dni']
+        self.nombre_usuario = formulario_html['nombre_usuario']
+        self.apellido_usuario = formulario_html['apellido_usuario']
+        self.mail_usuario = formulario_html['mail_usuario']
+        self.alias = formulario_html['alias']
+        self.contrasenia = formulario_html['contrasenia']
+
+
+        
+    
+
